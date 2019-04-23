dbfbe227d218ced9dc703f53b034b03957f31e36
ybx <mr_ybx@qq.com>
2019-04-23 18:28:08 +0800

    first commit of the code

---
+++ imitation.py
@@ -0,0 +1,215 @@
+import torch
+import torchvision
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+from torch.utils.data import Dataset
+from conf import Path
+from sklearn.model_selection import train_test_split
+
+class VideoDataset(Dataset):
+
+    def __init__(self, dataset='hmdb51', split='train', clip_len=30, preprocess=False):
+        self.root_dir, self.output_dir = Path.db_dir(dataset)
+        folder = os.path.join(self.output_dir, split)
+        self.clip_len = clip_len
+        self.split = split
+
+        self.resize_height = 299
+        self.resize_width = 299
+        self.crop_size = 112  ##
+
+
+        if not self.check_integrity():
+            raise RuntimeError('Dataset not found')
+        if (not self.check_preprocess()) or preprocess:
+            print('preprocessing the dataset ...')
+            self.preprocess()
+
+        self.fnames, labels = [], []
+        for label in sorted(os.listdir(folder)):
+            for fname in os.listdir(os.path.join(folder, label)):
+                self.fnames.append(os.path.join(folder, label, fname))
+                labels.append(label)
+
+        assert len(labels) == len(self.fnames)
+        print('num of {} videos: {:d}'.format(split, len(self.fnames)))
+
+        self.label2index = {label: index for index, label in enumerate(sorted(set(labels)))}
+        self.label_array = np.array([self.label2index[label] for label in labels], dtype=int)
+
+        if dataset == "ucf101":
+            if not os.path.exists('./ucf_labels.txt'):
+                with open('./ucf_labels.txt', 'w') as f:
+                    for id, label in enumerate(sorted(self.label2index)):
+                        f.writelines(str(id + 1) + ' ' + label + '\n')
+        elif dataset == 'hmdb51':
+            if not os.path.exists('./hmdb_labels.txt'):
+                with open('./hmdb_labels.txt', 'w') as f:
+                    for id, label in enumerate(sorted(self.label2index)):
+                        f.writelines(str(id) + ' ' + label + '\n')
+
+    def __len__(self):
+        return len(self.fnames)
+
+    def __getitem__(self, index):
+        buffer = self.load_frames(self.fnames[index])
+        #buffer = self.crop(buffer, self.clip_len, self.crop_size)
+        labels = np.array(self.label_array[index])
+
+        if self.split == 'test':
+            buffer = self.randomflip(buffer)
+        buffer = self.normalize(buffer)
+        buffer = self.to_tensor(buffer)
+        return torch.from_numpy(buffer), torch.from_numpy(labels)
+
+    def check_integrity(self):
+        if not os.path.exists(self.root_dir):
+            return False
+        else:
+            return True
+
+    def check_preprocess(self):
+        if not os.path.exists(self.output_dir):
+            return False
+        elif not os.path.exists(os.path.join(self.output_dir, 'train')):
+            return False
+
+        return True
+
+    def preprocess(self):
+        if not os.path.exists(self.output_dir):
+            os.mkdir(self.output_dir)
+            os.mkdir(os.path.join(self.output_dir, 'train'))
+            os.mkdir(os.path.join(self.output_dir, 'valid'))
+            os.mkdir(os.path.join(self.output_dir, 'test'))
+
+        for file in os.listdir(self.root_dir):
+            file_path = os.path.join(self.root_dir, file)
+            video_files = [name for name in os.listdir(file_path)]
+
+            train_and_valid, test = train_test_split(video_files, test_size=0.2, random_state=42)
+            train, val = train_test_split(train_and_valid, test_size=0.2, random_state=42)
+
+            train_dir = os.path.join(self.output_dir, 'train', file)
+            val_dir = os.path.join(self.output_dir, 'valid', file)
+            test_dir = os.path.join(self.output_dir, 'test', file)
+
+            if not os.path.exists(train_dir):
+                os.mkdir(train_dir)
+            if not os.path.exists(val_dir):
+                os.mkdir(val_dir)
+            if not os.path.exists(test_dir):
+                os.mkdir(test_dir)
+
+            for video in train:
+                self.process_video(video, file, train_dir)
+            for video in val:
+                self.process_video(video, file, val_dir)
+            for video in test:
+                self.process_video(video, file, test_dir)
+
+    def process_video(self, video, action_name, save_dir):
+        video_filename = video.split('.')[0]
+        if not os.path.exists(os.path.join(save_dir, video_filename)):
+            os.mkdir(os.path.join(save_dir, video_filename))
+
+        capture = cv2.VideoCapture(os.path.join(self.root_dir, action_name, video))
+        frame_count = int(capture.get(cv2.CAP_PROP_FRAME_COUNT))
+        frame_width = int(capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+        frame_height = int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+        skip = frame_count // self.clip_len
+        if skip == 0:
+            skip = 1
+        count = 0
+        i = 0
+        success = True
+        while (count < frame_count and success):
+            success, frame = capture.read()
+            if frame is None:
+                continue
+            if count % skip == 0:
+                frame = cv2.resize(frame, (self.resize_width, self.resize_height))
+                cv2.imwrite(os.path.join(save_dir, video_filename, '00{}.jpg'.format(str(i))), frame)
+                i += 1
+            count += 1
+        capture.release()
+
+    def load_frames(self, file_dir):
+        frames = sorted([os.path.join(file_dir, img) for img in os.listdir(file_dir)])
+        #frame_count = len(frames)
+        buffer = np.empty((self.clip_len, self.resize_height, self.resize_width,3), np.dtype('float32'))
+        for i, frame_name in enumerate(frames):
+            frame = np.array(cv2.imread(frame_name)).astype(np.float64)
+            buffer[i] = frame
+            if i == 29:
+                break
+        return buffer
+    #返回类型timestep*H*W*C
+
+    def randomflip(self, buffer):
+        if np.random.random() < 0.5:
+            for i, frame in enumerate(buffer):
+                frame = cv2.flip(buffer[i], flipCode=1)
+                buffer[i] = cv2.flip(frame, flipCode=1)
+        return buffer
+
+    def normalize(self, buffer):
+        for i, frame in enumerate(buffer):
+            frame -= np.array([[[90.0,98.0,102.0]]])
+            buffer[i] = frame
+        return buffer
+
+
+    def to_tensor(self, buffer):
+        return buffer.transpose((3,0,1,2))
+        #返回C*timestep*H*W
+        #imread是B*G*R
+
+    def get_map(self):
+        return self.label2index
+
+#用来验证label和input的对应关系
+def imshow(tensor, title=None):
+    unloader = torchvision.transforms.ToPILImage()
+    image = tensor.cpu().clone()
+    #print(image.size())
+    index = [2,0,1]#BGR->RGB
+    image = image[index]
+    #b,g,r = cv2.split(image)
+    #image = cv2.merge([r.g.b])
+    #image =image.float()
+    image = unloader(image)
+    plt.imshow(image)
+    plt.pause(0.001)
+
+def get_keys(dict, value):
+    for k,v in dict.items():
+        if v == value:
+            return k
+
+if __name__ == "__main__":
+    from torch.utils.data import DataLoader
+    train_data =VideoDataset(dataset='hmdb51', split='train', clip_len=30, preprocess=False)
+    #print(len(train_data))
+    train_loader = DataLoader(train_data, batch_size=10, shuffle=True, num_workers=4)
+
+    my_map = train_data.get_map()
+    for i, sample in enumerate(train_loader):
+        inputs = sample[0]
+        labels = sample[1]
+        print(inputs.size())
+        print(labels)
+
+        if i == 0:
+            break
+    for j in range(inputs.size(0)):
+        for k in range(inputs.size(2)):
+            img = inputs[j,:,k,:,:]
+            #print(img.size())
+            imshow(img)
+            label = get_keys(my_map,labels[j].item())
+            print("class:",label)
+
---
+++ lstm.py
@@ -0,0 +1,190 @@
+import torch
+import torch.nn as nn
+import torchvision
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+class lstm_cell(nn.Module):
+    def __init__(self, input_num, hidden_num):
+        super(lstm_cell, self).__init__()
+
+        self.input_num = input_num
+        self.hidden_num = hidden_num
+
+        self.Wxi = nn.Linear(self.input_num, self.hidden_num, bias=True)
+        self.Whi = nn.Linear(self.hidden_num, self.hidden_num, bias=False)
+        self.Wxf = nn.Linear(self.input_num, self.hidden_num, bias=True)
+        self.Whf = nn.Linear(self.hidden_num, self.hidden_num, bias=False)
+        self.Wxc = nn.Linear(self.input_num, self.hidden_num, bias=True)
+        self.Whc = nn.Linear(self.hidden_num, self.hidden_num, bias=False)
+        self.Wxo = nn.Linear(self.input_num, self.hidden_num, bias=True)
+        self.Who = nn.Linear(self.hidden_num, self.hidden_num, bias=False)
+
+    def forward(self, xt, ht_1, ct_1): #xt即为输入数据
+        xt = xt.to(device)
+        ht_1 = ht_1.to(device)
+        ct_1 = ct_1.to(device)
+        it = torch.sigmoid(self.Wxi(xt) + self.Whi(ht_1))
+        ft = torch.sigmoid(self.Wxf(xt) + self.Whf(ht_1))
+        ot = torch.sigmoid(self.Wxo(xt) + self.Who(ht_1))
+        ct = ft * ct_1 + it * torch.tanh(self.Wxc(xt) + self.Whc(ht_1))
+        ht = ot * torch.tanh(ct)
+        return  ht, ct
+
+
+class ALSTM(nn.Module):
+
+    def __init__(self, input_num, hidden_num, num_layers,out_num ):
+        super(ALSTM, self).__init__()
+        # Make sure that `hidden_num` are lists having len == num_layers
+        hidden_num = self._extend_for_multilayer(hidden_num, num_layers)
+
+        if not len(hidden_num) == num_layers:
+            raise ValueError('The length of hidden_num is not consistent with num_layers.')
+
+        self.input_num = input_num
+        self.hidden_num = hidden_num
+        self.num_layers = num_layers
+        self.out_num = out_num
+
+        cell_list = []
+        for i in range(0, self.num_layers):
+            cur_input_num = self.input_num if i == 0 else self.hidden_num[i - 1]
+            cell_list.append(lstm_cell(cur_input_num,self.hidden_num[i]))
+
+        self.cell_list = nn.ModuleList(cell_list)
+        self.conv=nn.Sequential(*list(torchvision.models.resnet101().children())[:-2])
+        for param in self.conv.parameters():
+            param.requires_grad = False
+        self.conv=nn.Sequential(*list(torchvision.models.resnet101(pretrained=True).children())[:-2])
+        self.Wha=nn.Linear(self.hidden_num[-1],49)
+        self.fc=nn.Linear(self.hidden_num[-1],self.out_num)
+        self.softmax=nn.Softmax(dim=1)
+
+    def forward(self, x, hidden_state=None):
+        #input model: batchsize x channel x seq_len x height x width
+        #input size: 30 x 224 x 224 for googLeNet
+        # init -1 time hidden units
+        if hidden_state is not None:
+            raise NotImplementedError()
+        else:
+            hidden_state = self._init_hidden(batch_size=x.size(0))
+
+        out_list=[]
+        seq_len = x.size(2) #seq输入的长度，即帧数
+        feature_cube = []
+        for t in range(seq_len):
+            output_t = []
+            for layer_idx in range(self.num_layers):
+                if 0==t:
+                    ht_1, ct_1 = hidden_state[layer_idx][0],hidden_state[layer_idx][1]
+                    attention_h=hidden_state[-1][0].to(device)
+                else:
+                    ht_1, ct_1 = hct_1[layer_idx][0],hct_1[layer_idx][1]
+                if 0==layer_idx:
+                    feature_map=self.conv(x[:,:,t,:,:].to(device))
+                    feature_map=feature_map.view(feature_map.size(0),feature_map.size(1),-1)
+                    attention_map=self.Wha(attention_h)
+                    attention_map=torch.unsqueeze(self.softmax(attention_map),1)
+                    #利用spatial attention对提取到的特征进行加权求和
+                    attention_feature=attention_map*feature_map #batchsize*2048*49
+                    attention_feature=torch.sum(attention_feature,2) #batchsize*2048
+                    ht, ct = self.cell_list[layer_idx](attention_feature,ht_1, ct_1)
+                    output_t.append([ht,ct])
+                else:
+                    ht, ct = self.cell_list[layer_idx](output_t[layer_idx-1][0], ht_1, ct_1)
+                    output_t.append([ht,ct]) #把ht和ct作为输出存储
+            attention_h=output_t[-1][0] #取最后一层的ht作为attention_h
+            hct_1=output_t
+            feature_cube.append(attention_feature)
+            out_list.append(self.fc(output_t[-1][0])) #将最后一层的ht通过全连接层得到输出yt
+        #seq_len*batchsize*2048 and
+        return torch.stack(feature_cube,0), torch.stack(out_list,1)#在第二个维度上连接out_list中的tensor，把每个time_setp中获得的输出yt堆叠起来作为总体输出
+
+    def _init_hidden(self, batch_size):
+        init_states = []
+        for i in range(self.num_layers):
+            init_states.append([torch.zeros(batch_size, self.hidden_num[i]),torch.zeros(batch_size, self.hidden_num[i])])
+        return init_states
+
+    @staticmethod
+    def _extend_for_multilayer(param, num_layers):
+        if not isinstance(param, list):
+            param = [param] * num_layers
+        return param
+
+
+class saliency_evaluater(nn.Module):
+    def __init__(self, hidden_size, bidirectional=True):
+        super(saliency_evaluater,self).__init__()
+        #weight_size: 用于生成attention的全连接层的input_size
+        self.weight_size = hidden_size*2 if bidirectional else hidden_size
+        self.fc = nn.Linear(self.weight_size, 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        attention = self.sigmoid(self.fc(x))
+        return attention.squeeze(2)
+
+
+class temp_att_net(nn.Module):
+    def __init__(self,input_size, hidden_size, num_layers, bidirectional=True, dropout=0.0):
+        super(temp_att_net,self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.directions = 2 if bidirectional else 1
+        self.net = nn.RNN(input_size, hidden_size, num_layers, bidirectional=bidirectional,dropout=dropout)
+        #self.dropout = nn.Dropout(dropout)
+        self.saliency_evaluater = saliency_evaluater(self.hidden_size, bidirectional)
+
+    def forward(self, x):
+        # x:  seq_len*batch_size*input_size
+        #初始化h0,其shape为(num_layers*directions)*batch_size*hidden_size
+        self.h_0 = torch.zeros(self.num_layers*self.directions, x.size(1), self.hidden_size).to(device)
+        #输入到RNN网络中，这里y包括了所有time_step的输出，h_t则只是最后time-step的隐层状态，双向情况下则是两端的隐层状态
+        y, h_t = self.net(x, self.h_0)
+        attention = self.saliency_evaluater(y)
+        return attention
+
+
+class spatio_temp_model(nn.Module):
+    def __init__(self, input_size, lstm_hidden_size, rnn_hidden_size, out_num, num_lstm_layers=1, num_rnn_layers=1):
+        super(spatio_temp_model,self).__init__()
+        self.spatio_att_net = ALSTM(input_size, lstm_hidden_size, num_lstm_layers, out_num)
+        self.temp_att_net = temp_att_net(input_size, rnn_hidden_size, num_rnn_layers)
+
+    def forward(self, x):
+        # shape: batchsize*channel*seq_len*height*width
+        feature, out = self.spatio_att_net(x)
+        temp_att = self.temp_att_net(feature)  #seq_len*batchsize
+        normalized_temp_att = nn.Softmax(dim=1)(torch.transpose(temp_att, 0, 1)) #normalization
+        weighted_pred = torch.mul(normalized_temp_att.unsqueeze(2), out)
+
+        return weighted_pred
+
+
+if __name__ == "__main__":
+    #若效果不好尝试attention_weight
+    #改变初始化策略
+    #多层+dropout
+    #数据增强
+
+    #batchsize*channel*seq_len*height*width
+    """
+    inputs = torch.randn(2, 3, 4, 224, 224)
+    net = ALSTM(2048, 256, 1, 51)
+    net = net.to(device)
+    feature, out = net.forward(inputs)
+    print("size of feature",feature.size())
+    print("size of out",out.size()) # batchsize*seq_len*class_num
+
+    att_net = temp_att_net(2048,1024,1).to(device)
+    attention = att_net(feature) #seq_len*batchsize
+    attention = nn.Softmax(dim=1)(torch.transpose(attention, 0, 1)) #nomalize
+    weighted_pred = torch.mul(attention.unsqueeze(2), out) #以时间注意力作为权重
+    """
+    inputs = torch.randn(2, 3, 4, 224, 224).to(device)
+    model = spatio_temp_model(2048,256,51).to(device)
+    output = model(inputs)
+    print(output) #batchsize*seq_len*class_num
